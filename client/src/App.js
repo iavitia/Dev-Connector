3751ed74ec9b073e8fbf2bd0acab99abda055a86
iavitia <iavitia@outlook.com>
2018-12-16 03:20:47 -0700

    prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser, logoutUser } from './actions/authActions';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
-import './App.css';
+import "./App.css";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -32,7 +32,7 @@ if (localStorage.jwtToken) {
     // TODO: Clear current Profile
 
     // Redirect to login
-    window.location.href = '/login';
+    window.location.href = "/login";
   }
 }
 
