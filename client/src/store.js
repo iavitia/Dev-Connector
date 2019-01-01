c033da4fce5f2451a14eef033680f35d4ac451b8
iavitia <iavitia@outlook.com>
2019-01-01 13:12:30 -0700

    fix errors

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,26 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware)
+  // other store enhancers if any
+);
+const store = createStore(rootReducer, initialState, enhancer);
+
+/* const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-);
+); */
 
 export default store;
