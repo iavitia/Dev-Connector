febfa3ef3e26680fd87a270acbb071f880bd78d8
iavitia <iavitia@outlook.com>
2019-01-09 14:04:35 -0700

    prep for deploy

--- client/src/store.js
+++ client/src/store.js
@@ -1,3 +1,35 @@
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+// import rootReducer from "./reducers";
+
+// const initialState = {};
+
+// const middleware = [thunk];
+
+// const composeEnhancers =
+//   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//         // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+//       })
+//     : compose;
+
+// const enhancer = composeEnhancers(
+//   applyMiddleware(...middleware)
+//   // other store enhancers if any
+// );
+// const store = createStore(rootReducer, initialState, enhancer);
+
+// /* const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(thunk),
+//     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// ); */
+
+// export default store;
+
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
@@ -6,26 +38,18 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      })
+// Developer tools middleware
+const composeSetup =
+  process.env.NODE_ENV !== "production" &&
+  typeof window === "object" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : compose;
 
-const enhancer = composeEnhancers(
-  applyMiddleware(...middleware)
-  // other store enhancers if any
-);
-const store = createStore(rootReducer, initialState, enhancer);
-
-/* const store = createStore(
+const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(thunk),
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-); */
+  composeSetup(applyMiddleware(...middleware))
+);
 
 export default store;
