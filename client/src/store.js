491b90c1f3a8d69a683acb97d9be333fb0093063
iavitia <iavitia@outlook.com>
2019-01-09 14:14:47 -0700

    prep for deploy

--- client/src/store.js
+++ client/src/store.js
@@ -1,51 +1,18 @@
-// import { createStore, applyMiddleware, compose } from "redux";
-// import thunk from "redux-thunk";
-// import rootReducer from "./reducers";
-
-// const initialState = {};
-
-// const middleware = [thunk];
-
-// const composeEnhancers =
-//   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-//         // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-//       })
-//     : compose;
-
-// const enhancer = composeEnhancers(
-//   applyMiddleware(...middleware)
-//   // other store enhancers if any
-// );
-// const store = createStore(rootReducer, initialState, enhancer);
-
-// /* const store = createStore(
-//   rootReducer,
-//   initialState,
-//   compose(
-//     applyMiddleware(thunk),
-//     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// ); */
-
-// export default store;
-
-import { compose, createStore } from "redux";
-import { persistStore, autoRehydrate } from "redux-persist";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-// eslint-disable-next-line no-underscore-dangle
-let devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production") {
-  devTools = a => a;
-}
+const initialState = {};
+
+const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  undefined,
+  initialState,
   compose(
-    autoRehydrate(),
-    devTools
+    applyMiddleware(...middleware)
+    //  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
+export default store;
